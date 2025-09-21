c9a3659b74beddf39e0da24d0c74e4c527cb32c8
tushar1074 <tushargoswami405@gmail.com>
2025-09-21 17:51:39 +0530

    Initial commit - Real-Time Chat Application

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,33 @@
+package com.chat.app.model;
+
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+
+    public ChatMessage() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
